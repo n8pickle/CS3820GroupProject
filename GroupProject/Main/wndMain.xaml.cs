a61c5eb0ddb80e237009f8be107d06b4c248668e
Whitney Smith <whitneysmith6@mail.weber.edu>
2021-11-20 01:49:56 -0700

    added comments for main window interaction

--- GroupProject/Main/wndMain.xaml.cs
+++ GroupProject/Main/wndMain.xaml.cs
@@ -21,6 +21,12 @@ namespace GroupProject
     /// </summary>
     public partial class MainWindow : Window
     {
+        // The main window should allow the user to create new invoices, edit existing invoices, or delete existing invoices.
+        // There is a Menu control box in the top left corner to navigate to the search and items pages.
+        // There are buttons that will connect the logic and SQL classes for the main window.
+        // Button click events are passed to the logic which will execute the SQL statement.
+        // The items page will populate the combo box with items.
+        // Refresh items in combo box when window visibility changes.
         public MainWindow()
         {
             InitializeComponent();
